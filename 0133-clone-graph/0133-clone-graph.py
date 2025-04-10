19860b1089f2f14eea6702c49c99ee0cf7f5e377
Shashank Bhagavatula <111755071+sbhag3@users.noreply.github.com>
2025-04-10 09:50:02 -0500

    Time: 40 ms (22.56%), Space: 12.8 MB (24.95%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return node
+
+        visited = {}
+        queue = deque([node])
+        visited[node] = Node(node.val, [])
+        while queue:
+            n = queue.popleft()
+            for neighbor in n.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val, [])
+                    queue.append(neighbor)
+                visited[n].neighbors.append(visited[neighbor])
+        return visited[node]
+        
