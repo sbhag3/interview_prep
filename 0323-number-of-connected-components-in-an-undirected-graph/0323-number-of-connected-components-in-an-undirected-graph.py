81e09b90248b3608feedbe8e913854037dbe4813
Shashank Bhagavatula <111755071+sbhag3@users.noreply.github.com>
2025-04-10 10:01:42 -0500

    Time: 11 ms (57.32%), Space: 13.1 MB (91.34%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        if n <= 0:
+            return 0
+
+        if n == 1:
+            return 1
+
+        parent = [i for i in range(n)]
+        size = [1 for i in range(n)]
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+
+            if rootX == rootY:
+                return False
+
+            if size[rootX] <= size[rootY]:
+                parent[rootX] = rootY
+                size[rootY] += size[rootX]
+            else:
+                parent[rootY] = rootX
+                size[rootX] += size[rootY]
+
+            return True
+
+        for u, v in edges:
+            if union(u, v):
+                n -= 1
+
+        return n
+
+        
+        
