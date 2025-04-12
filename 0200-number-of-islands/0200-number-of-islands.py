bbe1276b44e438ca2b3cdafd418aeaeba453fa8f
Shashank Bhagavatula <111755071+sbhag3@users.noreply.github.com>
2025-04-12 13:59:25 -0500

    Time: 219 ms (80.51%), Space: 27.9 MB (52.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,29 +4,27 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid:
-            return 0
-        
-        def dfs(i, j, grid):
-            if (
-                i < 0 or j < 0 
-                or i >= len(grid) or j >= len(grid[0])
-                or grid[i][j] != "1"
-            ):
+        # for each one that I encounter
+        # run BFS/DFS to try to find the entire island
+        # return # of times this is done
+        def dfs(i, j):
+            if (i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == "0"):
                 return
             
-            grid[i][j] = 0
-            dfs(i - 1, j, grid)
-            dfs(i + 1, j, grid)
-            dfs(i, j - 1, grid)
-            dfs(i, j + 1, grid)
-            
+            grid[i][j] = "0"
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+
         ans = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    dfs(i, j, grid)
+                    dfs(i, j)
                     ans += 1
-                    
+
         return ans
+
+        
         
