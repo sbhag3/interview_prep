fa732a689a20398645a053b8c55b43ba27dca12f
Shashank Bhagavatula <111755071+sbhag3@users.noreply.github.com>
2025-04-12 14:39:12 -0500

    Time: 3 ms (96.55%), Space: 13.3 MB (84.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -5,15 +5,48 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        graph = defaultdict(list)
-        indegree = [0 for _ in range(numCourses)]
+        # total courses need to take is numCourses
 
-        for pre, course in prerequisites:
+        # [[1, 0]] courses = 2
+        # need to take course 0 to finish course 1
+
+        # [[1, 0], [0, 1]]
+        # can't because for first requirement
+        # need to take course 0 to finish course 1
+        # but for second requirement
+        # need to take course 1 to finish course 0
+        # cyclic dependency
+
+        # courses that we can do first
+        # represent the course dependencies as a graph
+
+        # we see that the courses we can take initially are the courses with indegree 0
+        # in this case, we can also see that as we start to finish these courses, there
+        # may be courses that also start to open up because all the prereqs are done
+
+        # topological traversal of the graph
+
+        # start with all nodes of indegree 0
+        # start to "remove" them from the graph
+        # if new nodes start to acquire an indegree of 0
+        # we iterate on those
+        # finally we see if the number of nodes we removed is exactly the number of courses
+        # needed to be taken
+
+        # time complexity: O(m + n), where m is the size of prereqs and n is numCourses
+        # space complexity: O(m + n), graph is O(m) space and indegree is O(n) space
+
+        # code:
+        graph = defaultdict(list)
+        indegree = [0 for i in range(numCourses)]
+
+        for course, pre in prerequisites:
             graph[pre].append(course)
             indegree[course] += 1
 
-        taken = 0
         queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+
+        taken = 0
         while queue:
             curr = queue.popleft()
             taken += 1
@@ -23,4 +56,3 @@ class Solution(object):
                     queue.append(nbr)
 
         return taken == numCourses
-        
